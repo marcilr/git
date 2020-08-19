586904ebb9a8437f0dfb3ecc0013481ef0e35da1
Raymond Marcil <raymond.marcil@alaska.gov>
2020-08-19 13:54:46 -0800

    Added https://github.com/marcilr/tex-oracle

--- update.bat
+++ update.bat
@@ -5,6 +5,19 @@ REM by Raymond E. Marcil <marcilr@gmail.com>
 REM
 REM Batch script to update repositories in present working directory.
 REM
+REM Wed Aug 19, 2020
+REM ================
+REM Currently checks out:
+REM
+REM apache             https://github.com/marcilr/apache
+REM apache2            https://github.com/marcilr/apache2
+REM ant                https://github.com/marcilr/ant
+REM debian             https://github.com/marcilr/debian
+REM httpd              https://github.com/marcilr/httpd
+REM parted             https://github.com/marcilr/parted
+REM virtualization     https://github.com/marcilr/virtualization
+REM vmware             https://github.com/marcilr/vmware
+REM
 REM Links
 REM =====
 REM How to recursively delete directory from command line in windows?
@@ -47,6 +60,22 @@ IF EXIST apache (
 )
 
 
+REM
+REM ================================ APACHE2 ================================
+REM ================================ APACHE2 ================================
+REM ================================ APACHE2 ================================
+REM
+IF EXIST apache2 (
+  ECHO "Updating apache2..."
+  ECHO "Running: git pull https://github.com/marcilr/apache2"
+  cd apache
+  git pull https://github.com/marcilr/apache2
+  cd ..
+) ELSE (
+  ECHO "Cloning apache2..."
+  git clone "https://github.com/marcilr/apache2" apache2
+)
+
 REM ================================= ANT ===================================
 REM ================================= ANT ===================================
 REM ================================= ANT ===================================
@@ -77,6 +106,40 @@ IF EXIST debian (
 ) ELSE (
   ECHO "Cloning debian..."
   git clone "https://github.com/marcilr/debian" debian
+)
+
+
+REM
+REM ================================ HTTPD ==================================
+REM ================================ HTTPD ==================================
+REM ================================ HTTPD ==================================
+REM
+IF EXIST httpd (
+  ECHO "Updating httpd..."
+  ECHO "Running: git pull https://github.com/marcilr/httpd"
+  cd debian
+  git pull https://github.com/marcilr/httpd
+  cd ..
+) ELSE (
+  ECHO "Cloning httpd..."
+  git clone "https://github.com/marcilr/httpd" httpd
+)
+
+
+REM
+REM ================================ ORACLE ==================================
+REM ================================ ORACLE ==================================
+REM ================================ ORACLE ==================================
+REM
+IF EXIST oracle (
+  ECHO "Updating oracle..."
+  ECHO "Running: git pull https://github.com/marcilr/tex-oracle oracle"
+  cd debian
+  git pull https://github.com/marcilr/tex-oracle oracle
+  cd ..
+) ELSE (
+  ECHO "Cloning oracle..."
+  git clone "https://github.com/marcilr/tex-oracle" oracle
 )
 
 
