10e284445e3074e8ed2816ca37338f7d103eefa9
Raymond Marcil <raymond.marcil@alaska.gov>
2020-08-19 13:24:30 -0800

    Removed subversion related items.  Now only git only. Added repo urls for apache and debian:
    https://github.com/marcilr/apache
    https://github.com/marcilr/debian

--- update.bat
+++ update.bat
@@ -24,60 +24,10 @@ set "PATH=%PATH%;C:\Program Files\Git\bin"
 REM Path to development git repositories
 set PATHDEV="file:///C:/Users/remarcil/OneDrive - State of Alaska/git-repos/dev"
 
-
-REM =================================== SVN =======================================
-REM =================================== SVN =======================================
-REM =================================== SVN =======================================
-
-REM Path to development subversion repositories
-set PATHDEV="file:///C:/Users/remarcil/OneDrive - State of Alaska/svn-repos/dev"
-
-REM svn binary directory
-set SVNDIR=C:\Program Files\TortoiseSVN\bin
-set SVN=svn.exe
-
-REM svn command to use
-set "SVNCMD=%SVNDIR%\%SVN%"
-
-echo SVNDIR=%SVNDIR%
-echo SVN=%SVN%
-
-REM Build subversion command from path and binary
-echo SVNCMD=%SVNCMD%
-
-REM
-REM Updated subversion repos in current working directory
-REM Surroun script call with 'Program Files" embedded space with "" to
-REM alleviate error:
-REM
-REM 'C:\Program' is not recognized as an internal or external command,
-REM  operable program or batch file.'
-REM
-
-set SVNCMD="C:\Program Files\TortoiseSVN\bin\svn.exe"
-echo SVNCMD=%SVNCMD%
-
-REM
-REM Test subversion command
-REM This works:
-REM
-REM %SVNCMD% --help
-
 REM
 REM To recursiovely remove directory from command line:
 REM   > rmdir <DIR> /s /q
 REM
-
-REM
-REM ================================ ORACLE ================================
-REM
-REM %SVNCMD% co "file:///C:/Users/remarcil/OneDrive - State of Alaska/svn-repos/dev/databases/oracle/oracle/trunk" oracle
-REM %SVNCMD% co "file:///C:/Users/remarcil/OneDrive - State of Alaska/svn-repos/dev/databases/oracle/create/trunk" create
-
-REM
-REM ============================= POWERDESIGNER ============================
-REM
-REM %SVNCMD% co "file:///C:/Users/remarcil/OneDrive - State of Alaska/svn-repos/dev/ide/powerdesigner/powerdesigner/trunk" powerdesigner
 
 
 REM
@@ -88,7 +38,7 @@ REM
 IF EXIST apache (
   ECHO "Updating apache..."
   ECHO "Running: git pull https://github.com/marcilr/apache"
-  cd xterm
+  cd apache
   git pull https://github.com/marcilr/apache
   cd ..
 ) ELSE (
@@ -96,4 +46,18 @@ IF EXIST apache (
   git clone "https://github.com/marcilr/apache" apache
 )
 
-
+REM
+REM ================================ DEBIAN =================================
+REM ================================ DEBIAN =================================
+REM ================================ DEBIAN =================================
+REM
+IF EXIST debian (
+  ECHO "Updating debian..."
+  ECHO "Running: git pull https://github.com/marcilr/debian"
+  cd debian
+  git pull https://github.com/marcilr/debian
+  cd ..
+) ELSE (
+  ECHO "Cloning debian..."
+  git clone "https://github.com/marcilr/debian" debian
+)
